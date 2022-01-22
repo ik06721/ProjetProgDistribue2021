f341345e8f1d23d5edc63515855d5f99e33fb78a
ik06721 <73953504+ik06721@users.noreply.github.com>
2022-01-22 19:16:16 +0100

    Version 0.1.1

--- microservice-produits/src/main/resources/data.sql
+++ microservice-produits/src/main/resources/data.sql
@@ -1,7 +1,7 @@
-INSERT INTO product VALUES (0, 'Originale',' ', 'pizzaOriginale.png', 7);
+INSERT INTO product VALUES (0, 'Originale','Sauce tomate, mozzarella & origan ', 'pizzaOriginale.png', 7);
 
-INSERT INTO product VALUES (1, 'Hawaienne', ' ','pizzaHawaiian.png', 8);
+INSERT INTO product VALUES (1, 'Hawaienne', 'Sauce tomate, mozzarella, jambon & ananas ','pizzaHawaiian.png', 8);
 
-INSERT INTO product VALUES (2, 'Poulet', ' ', 'pizzaPoulet.png', 8);
+INSERT INTO product VALUES (2, 'Poulet', 'Sauce tomate, mozzarella, émincé de poulet & sauce BBQ  ', 'pizzaPoulet.png', 8);
 
-INSERT INTO product VALUES (3, 'Salami', ' ', 'pizzaSalami.png', 9);
+INSERT INTO product VALUES (3, 'Salami', 'Sauce tomate, mozzarella & salami doux', 'pizzaSalami.png', 9);
