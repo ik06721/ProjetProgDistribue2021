d97c6f7431ff95a08dc6a4b18b3814a175c1e788
ik06721 <73953504+ik06721@users.noreply.github.com>
2022-01-22 18:07:17 +0100

    Version 0.0.9
    Mise à jour du FrontEnd (rajout des fichiers CSS et JS pour la mise en forme)

--- microservice-produits/src/main/resources/data.sql
+++ microservice-produits/src/main/resources/data.sql
@@ -1,15 +1,7 @@
-INSERT INTO product VALUES (0, 'Bougie fonctionnant au feu', 'bougie qui fonctionne comme une ampoule mais sans électricité !', 'https://s3.eu-west-3.amazonaws.com/oc-images-cours/bougies.jpg', 22);
+INSERT INTO product VALUES (0, 'Originale',' ', 'pizzaOriginale.png', 7);
 
-INSERT INTO product VALUES (1, 'Chaise pour s''assoire', 'Chaise rare avec non pas 1 ni 2 mais 3 pieds', 'https://s3.eu-west-3.amazonaws.com/oc-images-cours/chaise.jpg', 95);
+INSERT INTO product VALUES (1, 'Hawaienne', ' ','pizzaHawaiian.png', 8);
 
-INSERT INTO product VALUES (2, 'Cheval pour nains', 'Ce cheval ne portera certainement pas blanche neige, mais sans problème les nains', 'https://s3.eu-west-3.amazonaws.com/oc-images-cours/cheval.jpg', 360);
+INSERT INTO product VALUES (2, 'Poulet', ' ', 'pizzaPoulet.png', 8);
 
-INSERT INTO product VALUES (3, 'Coq of steel, le superman des volailles', 'Ne passe pas au four', 'https://s3.eu-west-3.amazonaws.com/oc-images-cours/coq.jpg', 620);
-
-INSERT INTO product VALUES (4, 'Flacon à frotter avec un génie dedans', 'Vous donne droit à l''équivalent de 3/0 voeux', 'https://s3.eu-west-3.amazonaws.com/oc-images-cours/flacon.jpg', 1200);
-
-INSERT INTO product VALUES (5, 'Horloge quantique', 'Donne l''heure, les minutes et même les secondes. Ne fait pas de café', 'https://s3.eu-west-3.amazonaws.com/oc-images-cours/horloge.jpg', 180);
-
-INSERT INTO product VALUES (6, 'Table d''opération pour Hamsters', 'Pour réaliser vos opérations chirugicales sur votre Hamster!', 'https://s3.eu-west-3.amazonaws.com/oc-images-cours/table.jpg', 210);
-
-INSERT INTO product VALUES (7 , 'Vase ayant appartenu a Zeus', 'Risque de choc électrique', 'https://s3.eu-west-3.amazonaws.com/oc-images-cours/vase.jpg', 730);
+INSERT INTO product VALUES (3, 'Salami', ' ', 'pizzaSalami.png', 9);
